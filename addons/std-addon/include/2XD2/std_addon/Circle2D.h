b8b8d01709078bae5ee54a38a23623faadf1adb0
rune-suy <rune.suy@outlook.com>
2025-12-21 23:29:51 +0100

    Circle Node for std_addon

---
+++ addons/std-addon/include/2XD2/std_addon/Circle2D.h
@@ -0,0 +1,33 @@
+//
+// Created by rune-suy on 12/21/25.
+//
+
+#ifndef INC_2XD2_ENGINE_CIRCLE2D_H
+#define INC_2XD2_ENGINE_CIRCLE2D_H
+#include <SFML/Graphics/CircleShape.hpp>
+
+#include "2XD2/core/Node2D.h"
+
+
+namespace e2XD::std_addon
+{
+    class Circle2D : public core::Node2D
+    {
+        sf::CircleShape _shape;
+    public:
+        Circle2D()=default;
+        ~Circle2D()override=default;
+
+        void setRadius(float radius);
+
+        [[nodiscard]] float getRadius() const;
+
+        void setFillColor(const sf::Color& color);
+
+    protected:
+        void _internal_onDraw() override;
+    };
+} // std_addon
+// e2XD
+
+#endif //INC_2XD2_ENGINE_CIRCLE2D_H
---
+++ addons/std-addon/src/Circle2D.cpp
@@ -0,0 +1,40 @@
+//
+// Created by rune-suy on 12/21/25.
+//
+
+#include "2XD2/std_addon/Circle2D.h"
+#include "2XD2/renderer/Renderer.h"
+
+
+namespace e2XD::std_addon
+{
+    void Circle2D::_internal_onDraw()
+    {
+        const auto renderer = renderer::Renderer::getInstance();
+        const auto& position = getGlobalPosition();
+        _shape.setOrigin(_shape.getRadius() / 2, _shape.getRadius() / 2);
+        renderer->submit({
+            renderLayer,
+            &_shape,
+            {position.x, position.y},
+            zIndex
+        });
+        Node2D::_internal_onDraw();
+    }
+
+    float Circle2D::getRadius() const
+    {
+        return _shape.getRadius();
+    }
+
+    void Circle2D::setRadius(float radius)
+    {
+        _shape.setRadius(radius);
+    }
+
+    void Circle2D::setFillColor(const sf::Color& color)
+    {
+        _shape.setFillColor(color);
+    }
+} // std_addon
+// e2XD
