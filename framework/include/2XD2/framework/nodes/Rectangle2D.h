919773e5e43e2f8ee820f6304d71936b5e0804cf
rune-suy <rune.suy@outlook.com>
2025-12-21 17:18:49 +0100

    framework nodes docs

--- framework/include/2XD2/framework/nodes/Rectangle2D.h
+++ framework/include/2XD2/framework/nodes/Rectangle2D.h
@@ -10,6 +10,9 @@
 
 namespace e2XD::framework
 {
+    /**
+     * A 2D rectangle shape node.
+     */
     class Rectangle2D : public core::Node2D
     {
         sf::RectangleShape rectangleShape;
@@ -17,8 +20,16 @@ namespace e2XD::framework
         void _internal_onDraw() override;
 
     public:
+        /**
+         * Set the fill color of the rectangle.
+         * @param color
+         */
         void setFillColor(const sf::Color& color);
 
+        /**
+         * Set the size of the rectangle using game units.
+         * @param size
+         */
         void setSize(const core::Vec2& size);
     };
 } // framework
